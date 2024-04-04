b18b0316b9c125227a019a15af7d2fe0ceccf87c
YUNJINMOK <aprnaporn114@naver.com>
2024-04-04 10:09:55 +0900

    wow

--- src/components/Gravity.jsx
+++ src/components/Gravity.jsx
@@ -8,10 +8,10 @@ export default function Gravity() {
                     <h2 className="text-center mt-[10%] text-4xl font-bold">알레르기의 심각성</h2>
                 </div>
                 {/* 알레르기 심각성 뉴스 그리드 */}
-                <div className="flex flex-col xl:flex-row mt-16 xl:h-screen h-[1000px] justify-center items-center ">
+                <div className="flex flex-col md:flex-row mt-16 md:h-screen h-[1000px] justify-center items-center ">
                     {/* 왼쪽 그리드 */}
-                    <div className="xl:w-2/3 w-1/2 h-1/3 xl:h-full flex">
-                        <div className="w-[100%] xl:h-[67%] h-[100%]  flex justify-center items-center ">
+                    <div className="md:w-2/3 w-1/2 h-1/3 md:h-full flex">
+                        <div className="w-[100%] md:h-[67%] h-[100%]  flex justify-center items-center ">
                             <div className="w-[90%] h-[95%] bg-blue-200 rounded-2xl overflow-hidden">
                                 {/* 위쪽 이미지 */}
                                 <div className="w-full h-2/3 bg-red-200"></div>
@@ -21,8 +21,8 @@ export default function Gravity() {
                         </div>
                     </div>
                     {/* 오른쪽 그리드 */}
-                    <div className="xl:w-1/3 w-1/2 h-2/3 xl:h-full flex flex-col ">
-                        <div className="w-full xl:h-1/3 h-full  flex justify-center items-center">
+                    <div className="md:w-1/3 w-1/2 h-2/3 md:h-full flex flex-col ">
+                        <div className="w-full md:h-1/3 h-full  flex justify-center items-center">
                             <div className="w-[90%] h-[90%] bg-red-200 rounded-2xl overflow-hidden">
                                 {/* 위쪽 이미지 */}
                                 <div className="w-full h-2/3 bg-red-200"></div>
@@ -30,7 +30,7 @@ export default function Gravity() {
                                 <div className="w-full h-1/3 bg-blue-300"></div>
                             </div>
                         </div>
-                        <div className="w-full xl:h-1/3 h-full flex justify-center items-center">
+                        <div className="w-full md:h-1/3 h-full flex justify-center items-center">
                             <div className="w-[90%] h-[90%] bg-red-200 rounded-2xl overflow-hidden">
                                 {/* 위쪽 이미지 */}
                                 <div className="w-full h-2/3 bg-red-200"></div>
--- src/components/Merit.jsx
+++ src/components/Merit.jsx
@@ -4,16 +4,16 @@ export default function Merit() {
             {/* 컨테이너 */}
             <div id="container" className="w-full h-full max-w-[1600px] p-4">
                 {/* 통괄 */}
-                <div className="w-full h-full flex justify-center items-center">
+                <div className="w-full h-full flex flex-col xl:flex-row justify-center items-center">
                     {/* 왼쪽 소개글 */}
-                    <div className="w-2/3 h-full flex flex-col items-start justify-center  gap-4">
+                    <div className="xl:w-2/3 w-full xl:h-full flex flex-col xl:items-start items-center justify-center gap-4">
                         <div className="flex flex-col gap-6 m-[3%]">
                             <h2>우리 기업은 이러이러한 기술을</h2>
                             <h2 className="px-6">사용하여 알레르기 유발을 방지합니다.</h2>
                         </div>
                     </div>
                     {/* 오른쪽 이미지 */}
-                    <div className="w-1/3 h-[70%] bg-red-400 rounded-2xl">
+                    <div className="xl:w-1/3 w-1/2 h-[50%] xl:h-[70%] bg-red-400 rounded-2xl">
                         <div>zz</div>
                     </div>
                 </div>
