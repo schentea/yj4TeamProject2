4af0df76cc8372b22d7169f69c59d9e4b78c0782
schentea <schentea@gmail.com>
2024-04-04 18:44:54 +0900

    dsd

--- src/components/Merit.jsx
+++ src/components/Merit.jsx
@@ -6,7 +6,7 @@ export default function Merit() {
         {/* 통괄 */}
         <div className="w-full h-full flex flex-col justify-center items-center">
           {/* 위쪽 타이틀 */}
-          <div className="w-4/5 max-w-[1000px] xl:h-full flex flex-col xl:items-start items-center justify-center gap-4">
+          <div className="md:w-4/5 w-full max-w-[1000px] xl:h-full flex flex-col xl:items-start items-center justify-center gap-4">
             <div className="mt-[4%] mb-[6%] w-full ">
               <h2 className="font-bold xl:text-[48px] text-[#333C47]">
                 우리 기업은 알림톡 서비스를
@@ -17,7 +17,7 @@ export default function Merit() {
             </div>
           </div>
           {/* 위쪽 이미지 */}
-          <div className="w-full xl:h-[80vh] h-[40vh]  rounded-2xl flex">
+          <div className="w-full xl:h-[60vh] h-[40vh]  rounded-2xl flex flex-col md:flex-row justify-center items-center gap-4">
             {/* 왼쪽 사진 */}
             <div className="w-1/2 h-full  flex justify-center items-center">
               <img
@@ -27,17 +27,17 @@ export default function Merit() {
               />
             </div>
             {/* 오른쪽 글 */}
-            <div className="w-1/2 h-full  flex flex-col justify-center pl-8 ">
-              <h3 className="text-3xl font-semibold">
+            <div className="md:w-2/3 w-full h-full  flex flex-col md:items-baseline items-center justify-center xl:pl-8 ">
+              <h3 className="xl:text-3xl md:text-xl text-[20px] font-semibold">
                 식단표에 알레르기 성분이 검출되면
               </h3>
-              <h3 className="text-3xl font-semibold">
+              <h3 className=" xl:text-3xl md:text-xl text-[20px] font-semibold">
                 식단표를 붉은색으로 표시해 보기가 쉬워요!
               </h3>
             </div>
           </div>
           {/* 아래쪽 이미지 */}
-          <div className="w-full xl:h-[80vh] h-[40vh]  rounded-2xl flex mt-16">
+          <div className="w-full xl:h-[60vh] h-[40vh] mt-4  rounded-2xl flex flex-col md:flex-row justify-center items-center gap-4">
             {/* 왼쪽 사진 */}
             <div className="w-1/2 h-full  flex justify-center items-center">
               <img
@@ -47,10 +47,12 @@ export default function Merit() {
               />
             </div>
             {/* 오른쪽 글 */}
-            <div className="w-1/2 h-full  flex flex-col justify-center pl-8 ">
-              <h3 className="text-3xl font-semibold">알레르기 식품이 나오는</h3>
-              <h3 className="text-3xl font-semibold">
-                전날에는 알림톡을 보내드립니다!
+            <div className="md:w-2/3 w-full h-full  flex flex-col md:items-baseline items-center justify-center xl:pl-8 ">
+              <h3 className="xl:text-3xl md:text-xl text-[20px] font-semibold">
+                알레르기가 포함된 식품이 나오면
+              </h3>
+              <h3 className=" xl:text-3xl md:text-xl text-[20px] font-semibold">
+                바로전날 알림톡을 전송해드려요!
               </h3>
             </div>
           </div>
