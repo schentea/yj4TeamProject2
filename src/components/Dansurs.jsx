4e10a0d7cdc1b816bc1607c6db6ddf7b3abbc559
YUNJINMOK <aprnaporn114@naver.com>
2024-04-05 13:33:00 +0900

    dsd

--- src/components/Dansurs.jsx
+++ src/components/Dansurs.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import New1 from '../New1.png';
 import New2 from '../New2.png';
+import New3 from '../New3.png';
 import nail1 from '../sunmnail1.jpg';
 import nail2 from '../sunmnail2.jpg';
+import nail4 from '../sunmnail4.jpg';
 
 export default function Dansurs() {
     return (
@@ -31,12 +33,12 @@ export default function Dansurs() {
                                     <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
                                         <p className="text-[14px] font-semibold text-[#D39C4F]">알레르기 방지</p>
                                         <h3 className="text-lg font-semibold group-hover:underline">
-                                            세살 알레르기 여든까지 간다!
+                                            알레르기 없애는 법, 어릴 때부터 관리가 중요하다!
                                         </h3>
-                                        <h3 className="text-lg font-semibold group-hover:underline">
-                                            아이에게 알레르기가 있다면?
-                                        </h3>
-                                        <p className="text-[14px] text-gray-500"></p>
+
+                                        <p className="text-[14px] text-gray-500">
+                                            만성질환인만큼 꾸준한 치료가 필요한 소아 알레르기에 대해 알아볼까요?
+                                        </p>
                                     </div>
                                 </Link>
                             </div>
@@ -57,7 +59,7 @@ export default function Dansurs() {
                                             식품 알레르기에 대해 알아야 할 7가지
                                         </h3>
                                         <p className="text-[14px] text-gray-500">
-                                            반려견 산책 시 주의해야 할 식물들에 대해 알아보는 시간을 가져보겠습니다.
+                                            골치아픈 식품 알레르기! 필수로 알아야 할 7가지에 대해서 알아볼까요?
                                         </p>
                                     </div>
                                 </Link>
@@ -76,15 +78,22 @@ export default function Dansurs() {
                                     <img src={New1} alt="img" className="w-full h-full object-cover object-center " />
                                 </div>
                                 {/* 아래 내용 */}
-                                <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
-                                    <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
-                                    <h3 className="text-lg font-semibold group-hover:underline">
-                                        가을철 반려견과의 산책 시 조심해야 할 식물 10가지
-                                    </h3>
-                                    <p className="text-[14px] text-gray-500">
-                                        반려견 산책 시 주의해야 할 식물들에 대해 알아보는 시간을 가져보겠습니다.
-                                    </p>
-                                </div>
+                                <Link
+                                    to="https://imnews.imbc.com/replay/2019/nwdesk/article/5128145_28802.html"
+                                    className="text-black"
+                                >
+                                    <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
+                                        <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
+                                        <h3 className="text-lg font-semibold group-hover:underline">
+                                            '아몬드' 먹으면 안 되는 아이·· 포장지 봐선 몰라
+                                        </h3>
+                                        <p className="text-[14px] text-gray-500">
+                                            이처럼 중증 식품 알레르기는 10세 미만 영유아에게서 가장 많이 발생합니다.
+                                            그런데 정작 어린이집은 알레르기 관리 대상에서 빠져 있고, 표시해야 할
+                                            알레르기 성분도, 표시 의무 업체도 아직 제한적입니다.
+                                        </p>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                         {/* 아래쪽 기사 */}
@@ -96,15 +105,22 @@ export default function Dansurs() {
                                     <img src={New2} alt="img" className="w-full h-full object-cover object-center " />
                                 </div>
                                 {/* 아래 내용 */}
-                                <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
-                                    <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
-                                    <h3 className="text-lg font-semibold group-hover:underline">
-                                        가을철 반려견과의 산책 시 조심해야 할 식물 10가지
-                                    </h3>
-                                    <p className="text-[14px] text-gray-500">
-                                        반려견 산책 시 주의해야 할 식물들에 대해 알아보는 시간을 가져보겠습니다.
-                                    </p>
-                                </div>
+                                <Link
+                                    to="https://imnews.imbc.com/replay/2017/nwtoday/article/4454969_31369.html"
+                                    className="text-black"
+                                >
+                                    <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
+                                        <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
+                                        <h3 className="text-lg font-semibold group-hover:underline">
+                                            '유제품 알레르기' 유치원생, 샌드위치 먹고 사망
+                                        </h3>
+                                        <p className="text-[14px] text-gray-500">
+                                            유치원에서 나눠준 샌드위치를 먹은 이후 과민성 쇼크 증상을 보인 뒤 목숨을
+                                            잃은 건데요. 샌드위치에 들어 있는 치즈에 대해 알레르기 반응을 일으킨 것으로
+                                            알려졌습니다.
+                                        </p>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -117,18 +133,25 @@ export default function Dansurs() {
                                 <p className=" absolute -top-8 font-bold">안전</p>
                                 {/* 이미지 */}
                                 <div className="w-full h-2/5 ">
-                                    <img src={nail1} alt="img" className="w-full h-full object-cover object-center " />
+                                    <img src={New3} alt="img" className="w-full h-full object-cover object-center " />
                                 </div>
                                 {/* 아래 내용 */}
-                                <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
-                                    <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
-                                    <h3 className="text-lg font-semibold group-hover:underline">
-                                        가을철 반려견과의 산책 시 조심해야 할 식물 10가지
-                                    </h3>
-                                    <p className="text-[14px] text-gray-500">
-                                        반려견 산책 시 주의해야 할 식물들에 대해 알아보는 시간을 가져보겠습니다.
-                                    </p>
-                                </div>
+                                <Link
+                                    to="https://imnews.imbc.com/replay/2019/nwdesk/article/5128144_28802.html"
+                                    className="text-black"
+                                >
+                                    <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
+                                        <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
+                                        <h3 className="text-lg font-semibold group-hover:underline">
+                                            유난 떤다고요? ·· 잘못 먹으면 "숨도 못 쉬어요"
+                                        </h3>
+                                        <p className="text-[14px] text-gray-500">
+                                            식품 알레르기가 있는 아이들은 원인 물질을 먹게 되면 자칫 생명까지 위협받을
+                                            수 있는데요. 이런 자녀를 둔 부모들은 아이가 밖에서 무엇을 먹게 될지 몰라
+                                            전전 긍긍해야 합니다.
+                                        </p>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                         {/* 아래쪽 기사 */}
@@ -137,18 +160,24 @@ export default function Dansurs() {
                             <div className="w-[80%] relative  h-full ">
                                 {/* 이미지 */}
                                 <div className="w-full h-2/5 bg-blue-400">
-                                    <img src={nail2} alt="img" className="w-full h-full object-cover object-center " />
+                                    <img src={nail4} alt="img" className="w-full h-full object-cover object-center " />
                                 </div>
                                 {/* 아래 내용 */}
-                                <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
-                                    <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
-                                    <h3 className="text-lg font-semibold group-hover:underline">
-                                        가을철 반려견과의 산책 시 조심해야 할 식물 10가지
-                                    </h3>
-                                    <p className="text-[14px] text-gray-500">
-                                        반려견 산책 시 주의해야 할 식물들에 대해 알아보는 시간을 가져보겠습니다.
-                                    </p>
-                                </div>
+                                <Link
+                                    to="https://www.k-health.com/news/articleView.html?idxno=69985"
+                                    className="text-black"
+                                >
+                                    <div className=" absolute bottom-0 cursor-pointer duration-300 hover:h-4/6 w-full h-3/5 bg-white p-2 group">
+                                        <p className="text-[14px] font-semibold text-[#D39C4F]">생활안전</p>
+                                        <h3 className="text-lg font-semibold group-hover:underline">
+                                            설마 나도? 혹시 모를 ‘식품알레르기’, 이렇게 대비!
+                                        </h3>
+                                        <p className="text-[14px] text-gray-500">
+                                            알레르기 유발물질을 포함한 제품은 별도로 해당 재료를 표시하게 돼 있다.
+                                            식품알레르기가 있는 소비자라면 이를 꼭 확인 후 제품을 구입하는 것이 좋다
+                                        </p>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                     </div>
