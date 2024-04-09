d7a7d0cbccc3462bf318ce932587d4875ac45c3c
YUNJINMOK <aprnaporn114@naver.com>
2024-04-09 13:07:22 +0900

    login

---
+++ src/components/useUser.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { apiGetUser } from '../Api';
+
+export default function useUser() {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const Storage = sessionStorage.getItem('userData');
+        if (Storage) {
+            setUserData(JSON.parse(Storage));
+        }
+    }, []);
+    const { data } = useQuery(['getUser', { token: userData?.token }], apiGetUser);
+    console.log(data);
+    return userData || data;
+}
