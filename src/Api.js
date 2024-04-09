5e433d3c6cd90c3898f0382ee901df86861de9f3
YUNJINMOK <aprnaporn114@naver.com>
2024-04-09 11:09:23 +0900

    asd

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,10 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export async function apiPostUserRegister(data, selectedRegion, selectedAllergies) {
     // 데이터에 선택된 지역 추가
-    console.log(data, selectedRegion, selectedAllergies);
+    const postData = { ...data };
+    data.selectedRegion = selectedRegion;
+    data.selectedAllergies = selectedAllergies;
+    console.log(postData);
 
     try {
         return await fetch(`${BASE_URL}/users/register`, {
@@ -10,7 +13,7 @@ export async function apiPostUserRegister(data, selectedRegion, selectedAllergie
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(postData),
         }).then((res) => res.json());
     } catch (error) {
         console.log(error);
