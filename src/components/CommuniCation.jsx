a4e6b9b860c8871cb4f17eb73f690d23e6d8e9f8
schentea <schentea@gmail.com>
2024-04-15 08:22:54 +0900

    dsd

--- src/components/CommuniCation.jsx
+++ src/components/CommuniCation.jsx
@@ -40,7 +40,7 @@ export default function CommuniCation() {
         {/* 커뮤니케이션 타이틀 */}
         <div className="mt-[4%] text-center mb-[6%]">
           <h2 className="font-bold xl:text-[48px] text-[#333C47]">
-            원과 가정의 커뮤니케이션을 위한
+            학교와 가정의 커뮤니케이션을 위한
           </h2>
           <h2 className="font-bold xl:text-[48px] xl:mt-8 text-[#333C47]">
             최적의 솔류션, 케어키즈!
