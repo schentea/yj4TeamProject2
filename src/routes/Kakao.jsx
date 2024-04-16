9d1d00c9f2ff502cdea1a73a61606632b32ae328
YUNJINMOK <aprnaporn114@naver.com>
2024-04-16 16:38:57 +0900

    asd

--- src/routes/Kakao.jsx
+++ src/routes/Kakao.jsx
@@ -6,11 +6,15 @@ export default function Kakao() {
     const { search } = useLocation();
     const params = new URLSearchParams(search);
     const code = params.get('code');
+
     const { data } = useQuery(['getUser', { code }], apiKakaoLogin);
+    console.log('asd', data);
     const navigate = useNavigate();
     if (data?.result === true) {
+        sessionStorage.setItem('userData', JSON.stringify(data));
         navigate('/');
     }
+
     return (
         <div className="w-full flex flex-col items-center justify-center py-16">
             <div className="text-2xl font-bold">잠시만 기다려 주세요</div>
