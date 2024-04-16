8229da44a59e453b478b772f9673e86b24be542a
kwisdomkk <150204570+kwisdomkk@users.noreply.github.com>
2024-04-16 11:14:52 +0900

    DD

--- src/Api.js
+++ src/Api.js
@@ -7,6 +7,7 @@ const currentDate = new Date();
 const year = currentDate.getFullYear();
 const month = String(currentDate.getMonth() + 1).padStart(2, "0");
 const date = `${year}${month}`;
+// 세션에 저장된 데이터 가져오기
 let data;
 const Storage = sessionStorage.getItem("userData");
 if (Storage) {
