3c0c941a93c69b754bd4dda9bb2e06440ab87382
schentea <schentea@gmail.com>
2024-04-16 10:29:36 +0900

    dsd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,23 @@ import { FaInstagram } from "react-icons/fa";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { CiYoutube } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import logo from "../img/logo.svg";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[130px] bg-gray-500 flex justify-center">
+    <footer className="w-full h-[170px] bg-[#F9FAFB] flex justify-center">
       {/* Logo */}
       <div className="w-full max-w-[1200px] h-full p-5">
         <div className="h-full w-full flex justify-between items-center">
-          <div className="flex flex-col text-white font-bold text-[16px]">
-            <h6 className="font-bold">CareKids</h6>
+          <div className="flex flex-col text-black font-bold text-[16px] gap-3">
+            <img className="w-[180px]" src={logo} alt="sss" />
             <h6>대구광역시 동구 화랑로 525 </h6>
             <h6>Copyright ⓒ CcooK.,Ltd All Rights Reserved.</h6>
           </div>
 
-          <div className="flex space-x-4 h-full items-center text-white text-[24px]">
+          <div className="flex space-x-4 h-full items-center text-black text-[24px] gap-3">
             <p>
-              <Link to="https://www.instagram.com/" className="text-white">
+              <Link to="https://www.instagram.com/" className="text-black">
                 <FaInstagram />
               </Link>
             </p>
@@ -26,14 +27,14 @@ export default function Footer() {
               {" "}
               <Link
                 to="https://www.facebook.com/?locale=ko_KR"
-                className="text-white"
+                className="text-black"
               >
                 <FaSquareFacebook />
               </Link>
             </p>
             <p>
               {" "}
-              <Link to="https://www.youtube.com/" className="text-white">
+              <Link to="https://www.youtube.com/" className="text-black">
                 <CiYoutube />
               </Link>
             </p>
--- src/routes/SignupForm.jsx
+++ src/routes/SignupForm.jsx
@@ -8,7 +8,8 @@ import { useMutation } from "react-query";
 import { apiPostUserRegister, getSchoolInfo } from "../Api";
 import AllergyModalComponent from "./SignupAllergy";
 import { Link } from "react-router-dom";
-import { AiOutlineEyeInvisible,AiOutlineEye} from "react-icons/ai";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import logo from "../img/logo.svg";
 
 export default function SignupForm() {
   const [isRegionModalOpen, setIsRegionModalOpen] = useState(false);
@@ -111,15 +112,15 @@ export default function SignupForm() {
   const onValid = async (data) => {
     if (!Object.values(isChecked).includes(true)) {
       setIsSelected(true);
-      alert('지역을 선택해주세요.');
+      alert("지역을 선택해주세요.");
       return; // 지역 선택이 필요
     }
     if (!Object.values(isAllergyChecked).includes(true)) {
       setAllergy(true);
-      alert('알레르기 항목을 선택해주세요.');
+      alert("알레르기 항목을 선택해주세요.");
       return; // 알레르기 선택이 필요
     }
-  
+
     try {
       const schoolInfo = await getSchoolInfo(selectedRegion, data.schoolNM);
       if (!schoolInfo) {
@@ -128,16 +129,25 @@ export default function SignupForm() {
         });
         return;
       }
-  
+
       // API 호출
-      mutate({ data, selectedRegion, selectedAllergies: Object.keys(isAllergyChecked).filter(key => isAllergyChecked[key]) });
+      mutate({
+        data,
+        selectedRegion,
+        selectedAllergies: Object.keys(isAllergyChecked).filter(
+          (key) => isAllergyChecked[key]
+        ),
+      });
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   return (
-    <div id="form" className="w-full h-screen flex justify-center items-center p-4">
+    <div
+      id="form"
+      className="w-full h-screen flex justify-center items-center p-4"
+    >
       {/* 중앙 정렬 한 박스 */}
       <div
         className="w-full max-w-5xl h-[850px] flex rounded-lg overflow-hidden "
@@ -153,17 +163,26 @@ export default function SignupForm() {
         {/* 왼쪽 이미지 박스 */}
         <div className="w-1/2 relative h-full hidden md:block">
           {/* 배경 이미지 */}
-          <img className="w-full h-full object-cover object-center" src={Push} alt="FormImg" />
+          <img
+            className="w-full h-full object-cover object-center"
+            src={Push}
+            alt="FormImg"
+          />
           {/* 배경을 어둡게 처리 */}
           <div className="absolute inset-0 bg-black opacity-30"></div>
           {/* text  */}
 
           <Link to="/">
-            <span className=" text-white w-full text-left pl-4 text-3xl font-semibold absolute left-[50%] top-[2%] translate-x-[-50%] ">CareKids</span>
+            <span className=" text-white w-full text-left pl-4 text-3xl font-semibold absolute left-[50%] top-[2%] translate-x-[-50%] ">
+              <img className="w-[180px]" src={logo} alt="sss" />
+            </span>
           </Link>
         </div>
         {/* 오른쪽 로그인 박스 */}
-        <form className="md:w-1/2 w-full h-full px-10  flex flex-col gap-6 items-center " onSubmit={handleSubmit(onValid)}>
+        <form
+          className="md:w-1/2 w-full h-full px-10  flex flex-col gap-6 items-center "
+          onSubmit={handleSubmit(onValid)}
+        >
           <span className="text-2xl font-semibold py-4 h-4">회원가입</span>
           <div className="w-full h-[50px] relative">
             <input
@@ -178,7 +197,11 @@ export default function SignupForm() {
                 },
               })}
             />
-            {errors && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.userid?.message}</span>}
+            {errors && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.userid?.message}
+              </span>
+            )}
           </div>
           <div className="w-full h-[50px] relative">
             <input
@@ -193,7 +216,11 @@ export default function SignupForm() {
                 },
               })}
             />
-            {errors && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.username?.message}</span>}
+            {errors && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.username?.message}
+              </span>
+            )}
           </div>
           <div className="w-full h-[50px] relative">
             <input
@@ -203,12 +230,17 @@ export default function SignupForm() {
               {...register("email", {
                 required: "이메일은 필수 입력사항입니다",
                 pattern: {
-                  value: /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+                  value:
+                    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
                   message: "이메일 형식을 지켜주세요",
                 },
               })}
             />
-            {errors && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.email?.message}</span>}
+            {errors && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.email?.message}
+              </span>
+            )}
           </div>
           <div className="w-full h-[50px] relative">
             <input
@@ -223,8 +255,15 @@ export default function SignupForm() {
                 },
               })}
             />
-            {errors && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.password?.message}</span>}
-            <div onClick={togglePasswordVisibility} className="absolute top-4 right-4 cursor-pointer">
+            {errors && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.password?.message}
+              </span>
+            )}
+            <div
+              onClick={togglePasswordVisibility}
+              className="absolute top-4 right-4 cursor-pointer"
+            >
               {passwordVisible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
             </div>
           </div>
@@ -236,12 +275,21 @@ export default function SignupForm() {
               {...register("password2", {
                 required: "비밀번호 확인은 필수 입력사항입니다.",
                 validate: (value, form) => {
-                  return value === form.password || "비밀번호가 일치하지 않습니다.";
+                  return (
+                    value === form.password || "비밀번호가 일치하지 않습니다."
+                  );
                 },
               })}
             />
-            {errors && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.password2?.message}</span>}
-            <div onClick={togglePassword2Visibility} className="absolute top-4 right-4 cursor-pointer">
+            {errors && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.password2?.message}
+              </span>
+            )}
+            <div
+              onClick={togglePassword2Visibility}
+              className="absolute top-4 right-4 cursor-pointer"
+            >
               {password2Visible ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
             </div>
           </div>
@@ -254,17 +302,32 @@ export default function SignupForm() {
                 required: "전화번호는 필수 입력 사항입니다.",
                 pattern: {
                   value: /^\d{11}$/, // 숫자 11자리 패턴
-                  message: "전화번호는'-'을 제외한 11자리의 숫자로 입력해주세요.",
+                  message:
+                    "전화번호는'-'을 제외한 11자리의 숫자로 입력해주세요.",
                 },
               })}
             />
-            {errors && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.tel?.message}</span>}
+            {errors && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.tel?.message}
+              </span>
+            )}
           </div>
           <div className="w-full h-[50px] flex gap-2 ">
-            <div onClick={() => setIsRegionModalOpen(true)} className={`w-1/2 h-full border text-[13px] rounded-lg flex justify-center items-center font-semibold text-center bg-slate-100 cursor-pointer duration-500  hover:bg-slate-300 ${isSelected ? "text-red-500" : "text-black"}`}>
+            <div
+              onClick={() => setIsRegionModalOpen(true)}
+              className={`w-1/2 h-full border text-[13px] rounded-lg flex justify-center items-center font-semibold text-center bg-slate-100 cursor-pointer duration-500  hover:bg-slate-300 ${
+                isSelected ? "text-red-500" : "text-black"
+              }`}
+            >
               {isSelected ? "지역을 선택해주세요" : "지역을 선택해주세요"}
             </div>
-            <div onClick={() => setIsAllergyModalOpen(true)} className={`w-1/2 h-full border text-[13px] rounded-lg flex justify-center items-center font-semibold text-center bg-slate-100 cursor-pointer duration-500 hover:bg-slate-300 ${allergy ? "text-red-500" : "text-black"} `}>
+            <div
+              onClick={() => setIsAllergyModalOpen(true)}
+              className={`w-1/2 h-full border text-[13px] rounded-lg flex justify-center items-center font-semibold text-center bg-slate-100 cursor-pointer duration-500 hover:bg-slate-300 ${
+                allergy ? "text-red-500" : "text-black"
+              } `}
+            >
               알레르기 식품을 선택해주세요
             </div>
           </div>
@@ -277,7 +340,11 @@ export default function SignupForm() {
                 required: "학교명은 필수 입력 사항입니다.",
               })}
             />
-            {errors.schoolNM && <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">{errors?.schoolNM?.message}</span>}
+            {errors.schoolNM && (
+              <span className=" absolute -bottom-5 left-0 text-red-500 text-sm">
+                {errors?.schoolNM?.message}
+              </span>
+            )}
           </div>
           <button
             onClick={() => {
@@ -294,20 +361,38 @@ export default function SignupForm() {
           </button>
           <div className="w-full h-[55px] flex justify-center items-center gap-2">
             <div className="w-1/2 h-full bg-[#FEE500] rounded-lg md:h-[55px] flex justify-around items-center cursor-pointer">
-              <img className="w-[50px] h-[100%] cursor-pointer object-contain" src={Kakao} />
+              <img
+                className="w-[50px] h-[100%] cursor-pointer object-contain"
+                src={Kakao}
+              />
               <p className="flex items-center pt-[11px]">카카오 로그인</p>
               <span className="w-[10%]"></span>
             </div>
             <div className="w-1/2 h-full bg-[#F2F2F2] rounded-lg md:h-[55px] flex justify-around items-center cursor-pointer">
-              <img className="w-[50px] h-[100%] cursor-pointer object-contain" src={Google} />
+              <img
+                className="w-[50px] h-[100%] cursor-pointer object-contain"
+                src={Google}
+              />
               <p className="flex items-center pt-[11px]">구글 로그인</p>
               <span className="w-[10%]"></span>
             </div>
           </div>
         </form>
       </div>
-      {isRegionModalOpen && <ModalComponent closeModal={() => setIsRegionModalOpen(false)} isChecked={isChecked} handleRadioChange={handleRadioChange} />}
-      {isAllergyModalOpen && <AllergyModalComponent closeModal={() => setIsAllergyModalOpen(false)} isChecked={isAllergyChecked} handleCheckChange={handleCheckChange} />}
+      {isRegionModalOpen && (
+        <ModalComponent
+          closeModal={() => setIsRegionModalOpen(false)}
+          isChecked={isChecked}
+          handleRadioChange={handleRadioChange}
+        />
+      )}
+      {isAllergyModalOpen && (
+        <AllergyModalComponent
+          closeModal={() => setIsAllergyModalOpen(false)}
+          isChecked={isAllergyChecked}
+          handleCheckChange={handleCheckChange}
+        />
+      )}
     </div>
   );
 }
