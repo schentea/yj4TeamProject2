c0829a21ad16e6fd6180839ca8decf5ca86d8a18
YUNJINMOK <aprnaporn114@naver.com>
2024-04-16 14:04:08 +0900

    asd

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import NotFound from "./routes/NotFound";
-import SignupForm from "./routes/SignupForm";
-import LoginForm from "./routes/LoginForm";
-import MyPage from "./routes/MyPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import NotFound from './routes/NotFound';
+import SignupForm from './routes/SignupForm';
+import LoginForm from './routes/LoginForm';
+import MyPage from './routes/MyPage';
+import Kakao from './routes/Kakao';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Outlet />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: "",
-        element: <App />,
-      },
-      {
-        path: "/register",
-        element: <SignupForm />,
-      },
-      {
-        path: "/login",
-        element: <LoginForm />,
-      },
-      {
-        path: "/mypage",
-        element: <MyPage />,
-      },
-    ],
-  },
+    {
+        path: '/',
+        element: <Outlet />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: '',
+                element: <App />,
+            },
+            {
+                path: '/register',
+                element: <SignupForm />,
+            },
+            {
+                path: '/login',
+                element: <LoginForm />,
+            },
+            {
+                path: '/mypage',
+                element: <MyPage />,
+            },
+            {
+                path: 'socials/kakao',
+                element: <Kakao />,
+            },
+        ],
+    },
 ]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient();
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+    </QueryClientProvider>
 );
