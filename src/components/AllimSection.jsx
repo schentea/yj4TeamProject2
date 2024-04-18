9626bf1223592081355d94d49be6b11e344b4dc4
schentea <schentea@gmail.com>
2024-04-18 11:16:07 +0900

    ok

--- src/components/AllimSection.jsx
+++ src/components/AllimSection.jsx
@@ -2,8 +2,18 @@ import { apiAlimtalk } from "../Api";
 import "../Button.scss";
 import useUser from "./useUser";
 import icon from "../img/2Cheerful_happy_cute_girl_waving_raised_hand_to_say_hello_logo_hand_drawn_cartoon_art_illustration.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function AllimSection() {
+  const [modalOpen, setModalOpen] = useState(false);
   const userData = useUser();
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  console.log(userData);
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   const onClick = async () => {
     // 비동기 함수로 변경합니다.
@@ -65,9 +75,65 @@ export default function AllimSection() {
           <p className="text-[#666] text-sm w-full">
             학부모 소통, 급식 관리까지 효율적으로 관리할 수 있어요.{" "}
           </p>
-          <button class="w-1/2 btn btn-primary btn-jelly" onClick={onClick}>
+          <button class="w-1/2 btn btn-primary btn-jelly" onClick={openModal}>
             신청하기
           </button>
+          {modalOpen ? (
+            <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-10">
+              <div className="bg-white rounded-lg w-[400px] h-[600px] relative">
+                <div className="w-full h-12 flex justify-center items-center rounded-t-md bg-blue-400">
+                  <p className="text-white font-bold mt-3">
+                    입력하신 정보가 맞나요?
+                  </p>
+                </div>
+
+                <div
+                  className="absolute top-2 right-2 cursor-pointer"
+                  onClick={closeModal}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white hover:text-gray-800 transition duration-300 ease-in-out"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
+
+                <div className="p-4">
+                  <p className="text-lg">
+                    알레르기: {userData?.user.allergies}
+                  </p>
+                  <p className="text-lg">
+                    학교정보:{" "}
+                    {userData?.user.schoolNM &&
+                      userData?.user.schoolNM.split(",")[0]}
+                  </p>{" "}
+                  <p className="text-lg">전화번호: {userData?.user.tel}</p>
+                  <div className="w-full flex gap-2">
+                    <Link to="/mypage" className="w-1/2">
+                      <button class="w-full btn btn-primary btn-jelly">
+                        수정하기
+                      </button>
+                    </Link>
+                    <button
+                      class="w-1/2 btn btn-primary btn-jelly"
+                      onClick={onClick}
+                    >
+                      신청하기
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
         {/* 오른쪽 공간 */}
         <div className="w-1/2 h-1/2 hidden md:flex">
