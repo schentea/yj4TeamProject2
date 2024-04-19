c4acc2402cb13e58e2c81a951359f9ac9937a816
kwisdomkk <150204570+kwisdomkk@users.noreply.github.com>
2024-04-19 13:09:43 +0900

    32

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,52 +35,29 @@ export default function Header() {
             </Link>
             <div className="w-[60%] h-[200%] items-center justify-center hidden xl:flex pt-[15px]">
               <ul className="flex gap-5 items-center">
-                <a
-                  href="#section1"
-                  className="text-black relative no-underline text-[20px] font-semibold underline-hover duration-300"
-                >
+                <a href="#section1" className="text-black relative no-underline text-[20px] font-semibold underline-hover duration-300">
                   <p className="cursor-pointer text-black">커뮤니케이션</p>
                 </a>
-                <a
-                  href="#merit"
-                  className="text-black relative underline-hover no-underline text-[20px] font-semibold  duration-300"
-                >
+                <a href="#merit" className="text-black relative underline-hover no-underline text-[20px] font-semibold  duration-300">
                   <p className=" cursor-pointer">케즈키어 장점</p>
                 </a>
-                <a
-                  href="#dansurs"
-                  className="text-black  relative underline-hover no-underline text-[20px] font-semibold  duration-300"
-                >
+                <a href="#dansurs" className="text-black  relative underline-hover no-underline text-[20px] font-semibold  duration-300">
                   <p className=" cursor-pointer">심각성</p>
                 </a>
-                <a
-                  href="#methode"
-                  className="text-black  relative underline-hover no-underline text-[20px] font-semibold  duration-300"
-                >
+                <a href="#methode" className="text-black  relative underline-hover no-underline text-[20px] font-semibold  duration-300">
                   <p className=" cursor-pointer">사용방법</p>
                 </a>
-                <a
-                  href="#section2"
-                  className="text-black  relative underline-hover no-underline text-[20px] font-semibold  duration-300"
-                >
+                <a href="#section2" className="text-black  relative underline-hover no-underline text-[20px] font-semibold  duration-300">
                   <p className=" cursor-pointer">신청하기 </p>
                 </a>
               </ul>
             </div>
-            <div
-              id="Hidden_div"
-              className=" space-x-4 h-full items-center text-black font-bold justify-center mt-[7px] text-[20px] hidden xl:flex "
-            >
+            <div id="Hidden_div" className=" space-x-4 h-full items-center text-black font-bold justify-center mt-[7px] text-[20px] hidden xl:flex ">
               {userData ? (
                 <>
-                  <Link
-                    to="mypage"
-                    className=" h-full flex items-center no-underline text-black pb-3"
-                  >
+                  <Link to="mypage" className=" h-full flex items-center no-underline text-black pb-3">
                     <div className=" w-[60px] h-[60px] rounded-full flex items-center justify-center border border-white bg-gray-300 hover:bg-[#77BCFF] hover:text-white duration-500">
-                      <span>{`${userData.user.userid
-                        .substring(0, 2)
-                        .toUpperCase()}`}</span>
+                      <span>{`${userData.user.userid.substring(0, 2).toUpperCase()}`}</span>
                     </div>
                   </Link>
                   <p className="cursor-pointer" onClick={handleLogout}>
@@ -98,26 +75,13 @@ export default function Header() {
                 </>
               )}
             </div>
-            <button
-              id="main_button"
-              onClick={openButton}
-              className="text-black text-[30px] flex items-center cursor-pointer xl:hidden "
-            >
+            <button id="main_button" onClick={openButton} className="text-black text-[30px] flex items-center cursor-pointer xl:hidden ">
               <GiHamburgerMenu />
             </button>
             <AnimatePresence>
               {mobileOpen && (
-                <motion.div
-                  initial={{ x: "100vw" }}
-                  animate={{ x: "0" }}
-                  exit={{ x: "100vw" }}
-                  transition={{ duration: 0.5, ease: "easeOut" }}
-                  className="fixed top-0 right-0 bottom-0 w-1/2 z-30 h-full bg-[rgba(255,255,255,0.9)] p-5 font-bold text-[18px]"
-                >
-                  <motion.div
-                    onClick={closeButton}
-                    className=" cursor-pointer text-[30px] flex justify-end"
-                  >
+                <motion.div initial={{ x: "100vw" }} animate={{ x: "0" }} exit={{ x: "100vw" }} transition={{ duration: 0.5, ease: "easeOut" }} className="fixed top-0 right-0 bottom-0 w-1/2 z-30 h-full bg-[rgba(255,255,255,0.9)] p-5 font-bold text-[18px]">
+                  <motion.div onClick={closeButton} className=" cursor-pointer text-[30px] flex justify-end">
                     <IoClose />
                   </motion.div>
                   <div className="flex mb-8 mt-8">
@@ -133,12 +97,9 @@ export default function Header() {
                     ) : (
                       <>
                         <Link to="/login" className="text-black no-underline">
-                          <p>로그인</p>
+                          <p className="mr-4">로그인</p>
                         </Link>
-                        <Link
-                          to="/register"
-                          className="text-black no-underline"
-                        >
+                        <Link to="/register" className="text-black no-underline">
                           <p>회원가입</p>
                         </Link>
                       </>
@@ -150,10 +111,7 @@ export default function Header() {
                   <a href="#merit" className="text-black no-underline">
                     <p className=" cursor-pointer">우리 회사의 이점</p>
                   </a>
-                  <a
-                    href="#dansurs"
-                    className="text-black no-underline text-[20px] font-semibold"
-                  >
+                  <a href="#dansurs" className="text-black no-underline text-[20px] font-semibold">
                     <p className=" cursor-pointer">심각성</p>
                   </a>
                   <a href="#methode" className="text-black no-underline">
@@ -168,16 +126,10 @@ export default function Header() {
           </div>
         </div>
       </header>
-      <div
-        id="header"
-        className="w-full h-[145vh] bg-cover relative bg-center flex flex-col justify-center items-center"
-      >
+      <div id="header" className="w-full h-[145vh] bg-cover relative bg-center flex flex-col justify-center items-center">
         <div className="text-center absolute top-[25%]">
           <h1 className="fade-inheader md:text-7xl text-[40px] flex items-center justify-center">
-            <span
-              id="span"
-              className="font-bold text-[black] text-shodw-[-1px 0px 1px]"
-            >
+            <span id="span" className="font-bold text-[black] text-shodw-[-1px 0px 1px]">
               케어키즈
             </span>
             <span className="md:text-6xl mr-2 text-[#333C47]">는</span>{" "}
@@ -186,9 +138,7 @@ export default function Header() {
             </span>
             <span className="md:text-6xl text-[#333C47]">를</span>{" "}
           </h1>
-          <h2 className="fade-inheader md:text-4xl mt-4 text-[#333C47]">
-            가지고있는 아이들과 함께 걸어갑니다.
-          </h2>
+          <h2 className="fade-inheader md:text-4xl mt-4 text-[#333C47]">가지고있는 아이들과 함께 걸어갑니다.</h2>
         </div>
       </div>
     </>
--- src/routes/LoginForm.jsx
+++ src/routes/LoginForm.jsx
@@ -1,130 +1,111 @@
-import React from 'react';
-import Kakao from '../img/kakaoLogo.png';
-import Google from '../img/googleLogo.svg';
-import Push from '../img/push.jpg';
-import { useForm } from 'react-hook-form';
-import { useMutation, useQueryClient } from 'react-query';
-import { apiPostUserLogin } from '../Api';
-import { Link, useNavigate } from 'react-router-dom';
-import useUser from '../components/useUser';
-import { IoArrowBackOutline } from 'react-icons/io5';
+import React from "react";
+import Kakao from "../img/kakaoLogo.png";
+import Google from "../img/googleLogo.svg";
+import Push from "../img/push.jpg";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+import { apiPostUserLogin } from "../Api";
+import { Link, useNavigate } from "react-router-dom";
+import useUser from "../components/useUser";
+import { IoArrowBackOutline } from "react-icons/io5";
 export default function SignupForm() {
-    const kakaoUrl = 'https://kauth.kakao.com/oauth/authorize';
-    const config = {
-        response_type: 'code',
-        client_id: process.env.REACT_APP_KAKAO_CLIENT_ID,
-        redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
-    };
+  const kakaoUrl = "https://kauth.kakao.com/oauth/authorize";
+  const config = {
+    response_type: "code",
+    client_id: process.env.REACT_APP_KAKAO_CLIENT_ID,
+    redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
+  };
 
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${kakaoUrl}?${params}`;
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${kakaoUrl}?${params}`;
 
-    const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URL}&response_type=code&scope=email profile`;
+  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URL}&response_type=code&scope=email profile`;
 
-    const queryClient = useQueryClient();
-    const navigate = useNavigate();
-    const { mutate } = useMutation(apiPostUserLogin, {
-        onSuccess: (data) => {
-            if (data.result === true) {
-                // 서버에서 성공 여부를 확인
-                // 로그인 성공 시 세션 스토리지에 데이터 저장
-                sessionStorage.setItem('userData', JSON.stringify(data));
-                queryClient.invalidateQueries('getUser');
-                navigate('/');
-                window.location.reload();
-            } else {
-                // 로그인 실패 시
-                sessionStorage.clear();
-                navigate('/login');
-            }
-        },
-    });
-    const { register, handleSubmit } = useForm();
-    const onValid = (data) => {
-        mutate(data);
-    };
-    const data = useUser();
-    console.log(data);
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate } = useMutation(apiPostUserLogin, {
+    onSuccess: (data) => {
+      if (data.result === true) {
+        // 서버에서 성공 여부를 확인
+        // 로그인 성공 시 세션 스토리지에 데이터 저장
+        sessionStorage.setItem("userData", JSON.stringify(data));
+        queryClient.invalidateQueries("getUser");
+        navigate("/");
+        window.location.reload();
+      } else {
+        // 로그인 실패 시
+        sessionStorage.clear();
+        navigate("/login");
+      }
+    },
+  });
+  const { register, handleSubmit } = useForm();
+  const onValid = (data) => {
+    mutate(data);
+  };
+  const data = useUser();
+  console.log(data);
 
-    return (
-        <div id="form" className="w-full h-screen flex justify-center items-center p-4">
-            {/* 중앙 정렬 한 박스 */}
-            <div
-                className="w-full max-w-5xl h-[850px] flex rounded-lg overflow-hidden "
-                style={{
-                    background: '#7FC9EF',
-                    boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
-                    backdropFilter: 'blur(10px)',
-                    WebkitBackdropFilter: 'blur(10px)',
-                    borderRadius: '10px',
-                    border: '1px solid rgba(255, 255, 255, 0.18)',
-                }}
-            >
-                {/* 왼쪽 이미지 박스 */}
-                <div className="w-1/2 relative h-full hidden md:block">
-                    {/* 배경 이미지 */}
-                    <img className="w-full h-full object-cover object-center" src={Push} alt="FormImg" />
-                    {/* 배경을 어둡게 처리 */}
-                    <div className="absolute inset-0 bg-black opacity-30"></div>
-                    {/* text  */}
-                    <Link to="/">
-                        <span className=" text-white w-full text-left pl-4 text-3xl font-semibold fixed left-[50%] top-[2%] translate-x-[-50%] ">
-                            <IoArrowBackOutline />
-                        </span>
-                    </Link>
-                </div>
-                {/* 오른쪽 로그인 박스 */}
-                <form
-                    className="md:w-1/2 w-full h-full px-10  flex flex-col gap-3 items-center "
-                    onSubmit={handleSubmit(onValid)}
-                >
-                    <Link to="/">
-                        <span className="  block md:hidden text-black w-full text-left pl-4 text-3xl font-semibold fixed left-[50%] top-[2%] translate-x-[-50%] ">
-                            <IoArrowBackOutline />
-                        </span>
-                    </Link>
-                    <span className="text-2xl font-semibold py-8">로그인</span>
-                    <input
-                        className="w-full h-[50px] text-[14px] placeholder-up rounded-md px-2 py-2 outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent duration-500"
-                        type="text"
-                        placeholder="아이디"
-                        {...register('userid')}
-                    />
-
-                    <input
-                        className="w-full h-[50px] text-[14px] placeholder-up rounded-md px-2 py-2 outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent duration-500"
-                        type="password"
-                        placeholder="비밀번호"
-                        {...register('password')}
-                    />
-
-                    <div className="w-full h-[50px] flex gap-2 "></div>
-
-                    <button className="w-full h-[50px] border text-[17px] rounded-lg flex justify-center items-center font-bold text-center bg-slate-100 cursor-pointer duration-500 hover:bg-slate-300">
-                        로그인
-                    </button>
-                    <div className="w-full h-[55px] flex justify-center items-center gap-2">
-                        <div className="w-1/2 h-full bg-[#FEE500] rounded-lg md:h-[55px] flex justify-around items-center cursor-pointer">
-                            <img className="w-[50px] h-[100%] cursor-pointer object-contain" src={Kakao} alt="kakao" />
-                            <Link to={finalUrl}>
-                                <p className="flex items-center pt-[11px]">카카오 로그인</p>
-                            </Link>
-                            <span className="w-[10%]"></span>
-                        </div>
-                        <div className="w-1/2 h-full bg-[#F2F2F2] rounded-lg md:h-[55px] flex justify-around items-center cursor-pointer">
-                            <img
-                                className="w-[50px] h-[100%] cursor-pointer object-contain"
-                                src={Google}
-                                alt="google"
-                            />
-                            <Link to={googleUrl}>
-                                <p className="flex items-center pt-[11px]">구글 로그인</p>
-                            </Link>
-                            <span className="w-[10%]"></span>
-                        </div>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div id="form" className="w-full h-screen flex justify-center items-center p-4">
+      {/* 중앙 정렬 한 박스 */}
+      <div
+        className="w-full max-w-5xl h-[850px] flex rounded-lg overflow-hidden "
+        style={{
+          background: "#7FC9EF",
+          boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          backdropFilter: "blur(10px)",
+          WebkitBackdropFilter: "blur(10px)",
+          borderRadius: "10px",
+          border: "1px solid rgba(255, 255, 255, 0.18)",
+        }}
+      >
+        {/* 왼쪽 이미지 박스 */}
+        <div className="w-1/2 relative h-full hidden md:block">
+          {/* 배경 이미지 */}
+          <img className="w-full h-full object-cover object-center" src={Push} alt="FormImg" />
+          {/* 배경을 어둡게 처리 */}
+          <div className="absolute inset-0 bg-black opacity-30"></div>
+          {/* text  */}
+          <Link to="/">
+            <span className=" text-white w-full text-left pl-4 text-3xl font-semibold fixed left-[50%] top-[2%] translate-x-[-50%] ">
+              <IoArrowBackOutline />
+            </span>
+          </Link>
         </div>
-    );
+        {/* 오른쪽 로그인 박스 */}
+        <form className="md:w-1/2 w-full h-full px-10  flex flex-col gap-3 items-center " onSubmit={handleSubmit(onValid)}>
+          <Link to="/">
+            <span className="  block md:hidden text-black w-full text-left pl-4 text-3xl font-semibold fixed left-[50%] top-[2%] translate-x-[-50%] ">
+              <IoArrowBackOutline />
+            </span>
+          </Link>
+          <span className="text-2xl font-semibold py-8">로그인</span>
+          <input className="w-full h-[50px] text-[14px] placeholder-up rounded-md px-2 py-2 outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent duration-500" type="text" placeholder="아이디" {...register("userid")} />
+
+          <input className="w-full h-[50px] text-[14px] placeholder-up rounded-md px-2 py-2 outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent duration-500" type="password" placeholder="비밀번호" {...register("password")} />
+
+          <div className="w-full h-[50px] flex gap-2 "></div>
+
+          <button className="w-full h-[50px] border text-[17px] rounded-lg flex justify-center items-center font-bold text-center bg-slate-100 cursor-pointer duration-500 hover:bg-slate-300">로그인</button>
+          <div className="w-full h-[55px] flex justify-center items-center gap-2">
+            <div className="w-1/2 h-full bg-[#FEE500] rounded-lg md:h-[55px] flex justify-around items-center cursor-pointer">
+              <img className="w-[50px] h-[100%] cursor-pointer object-contain" src={Kakao} alt="kakao" />
+              <Link to={finalUrl} className=" no-underline text-black">
+                <p className="flex items-center pt-[11px]">카카오 로그인</p>
+              </Link>
+              <span className="w-[10%]"></span>
+            </div>
+            <div className="w-1/2 h-full bg-[#F2F2F2] rounded-lg md:h-[55px] flex justify-around items-center cursor-pointer">
+              <img className="w-[50px] h-[100%] cursor-pointer object-contain" src={Google} alt="google" />
+              <Link to={googleUrl} className=" no-underline text-black">
+                <p className="flex items-center pt-[11px]">구글 로그인</p>
+              </Link>
+              <span className="w-[10%]"></span>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
