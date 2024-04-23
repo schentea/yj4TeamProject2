005ab830647bf77649c09983137a2ce50ad9f4b8
schentea <schentea@gmail.com>
2024-04-23 15:53:39 +0900

    dsd

--- src/components/Use.jsx
+++ src/components/Use.jsx
@@ -1,10 +1,9 @@
 import section from "../img/section.svg";
-import section1 from "../img/section1.svg";
+
 export default function Use() {
   return (
     <div className="w-full h-full flex justify-center">
       <div className="w-full">
-        <img src={section1} alt="" />
         <img src={section} alt="" />
       </div>
     </div>
